b934e7d2c10af0c100f10d23f5143b27642c5301
Bishoy Sedra <bishosedra0@gmail.com>
2025-07-23 21:48:57 +0300

    Added ApplicationDbContext class to DataAccess Project!

---
+++ DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Models.Entities;
+
+namespace DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSets
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Apply all configurations from the Models project
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(Category).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
