76859a641927ea8bead2cc386defd04b8fec507a
Bishoy Sedra <bishosedra0@gmail.com>
2025-07-23 21:01:59 +0300

    Added Category Class in Models Project with its props!

---
+++ Models/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿namespace Models.Models
+{
+    public class Category
+    {
+        #region Properties
+        public int Id { get; set; }
+        public string catName { get; set; }
+
+        public int catOrder { get; set; }
+
+        public DateTime createdDate { get; set; }
+
+        public bool markedAsDeleted { get; set; }
+        #endregion
+    }
+}
