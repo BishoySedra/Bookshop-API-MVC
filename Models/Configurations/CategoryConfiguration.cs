93ad3846e742006a4c75978d34966da5554d4f89
Bishoy Sedra <bishosedra0@gmail.com>
2025-07-23 21:13:03 +0300

    Added configs using fluent API!

---
+++ Models/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Models.Models;
+
+namespace DataAccess.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            // Table and Schema
+            builder.ToTable("Categories", schema: "MasterSchema");
+
+            // Primary Key
+            builder.HasKey(c => c.Id);
+
+            // Property Configurations
+            builder.Property(c => c.catName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(c => c.catOrder)
+                .IsRequired();
+
+            builder.Ignore(c => c.createdDate);
+
+            builder.Property(c => c.markedAsDeleted)
+                .HasColumnName("isDeleted");
+        }
+    }
+}
