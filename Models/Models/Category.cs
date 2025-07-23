238c961608a6dd2f2fe5c156802d3ec6521878c4
Bishoy Sedra <bishosedra0@gmail.com>
2025-07-23 21:04:52 +0300

    Added DataAnnotations but making it commented!

--- Models/Models/Category.cs
+++ Models/Models/Category.cs
@@ -1,16 +1,29 @@
-﻿namespace Models.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models.Models
 {
     public class Category
     {
         #region Properties
+
+        // [Key] // --> Configured via Fluent API
         public int Id { get; set; }
+
+        // [Required]
+        // [MaxLength(50)]
         public string catName { get; set; }
 
+        // [Required]
         public int catOrder { get; set; }
 
+        // [NotMapped]
         public DateTime createdDate { get; set; }
 
+        // [Column("isDeleted")]
         public bool markedAsDeleted { get; set; }
+
         #endregion
     }
 }
