92fdee19b807e7259a8a4c604690767a55de6e06
Bishoy Sedra <bishosedra0@gmail.com>
2025-07-27 19:21:29 +0300

    Added one-to-many relationship from category to product + Applied a migration for that!

---
+++ DataAccess/Migrations/20250727161929_AddCategoryProductRelation.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryProductRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                schema: "MasterSchema",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                schema: "MasterSchema",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                schema: "MasterSchema",
+                table: "Products",
+                column: "CategoryId",
+                principalSchema: "MasterSchema",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                schema: "MasterSchema",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_CategoryId",
+                schema: "MasterSchema",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                schema: "MasterSchema",
+                table: "Products");
+        }
+    }
+}
--- Models/Entities/Product.cs
+++ Models/Entities/Product.cs
@@ -11,5 +11,10 @@
         public string Author { get; set; }
 
         public double Price { get; set; }
+
+        // Added this for the relationship for one-to-many with Category
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
     }
 }
