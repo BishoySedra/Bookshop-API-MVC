41bcc11a08202f8bfe00dac0ba454fde270fc1bb
Bishoy Sedra <bishosedra0@gmail.com>
2025-07-27 23:47:39 +0300

    Created some files to start implementing the views for products!

--- Web/Controllers/ProductViewController.cs
+++ Web/Controllers/ProductViewController.cs
@@ -1,92 +1,25 @@
-﻿using DataAccess;
-using DataAccess.Interfaces;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using DataAccess;
 using Models.Entities;
+using Web.ViewModels.Product;
 
 namespace Web.Controllers
 {
-    public class ProductViewController : Controller
-    {
-        private readonly IProductRepository _repo;
-        private readonly ApplicationDbContext _context;
+	public class ProductViewController : Controller
+	{
+		private readonly ApplicationDbContext _context;
 
-        public ProductViewController(IProductRepository repo, ApplicationDbContext context)
-        {
-            _repo = repo;
-            _context = context;
-        }
+		public ProductViewController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
 
-        public IActionResult Index()
-        {
-            var products = _repo.GetAll();
-            return View(products);
-        }
-
-        public IActionResult Create()
-        {
-            ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name");
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Create(Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                _repo.Create(product);
-                _repo.Save();
-                return RedirectToAction("Index");
-            }
-            ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name");
-            return View(product);
-        }
-
-        public IActionResult Edit(int id)
-        {
-            var product = _repo.GetById(id);
-            if (product == null) return NotFound();
-
-            ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
-            return View(product);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                _repo.Update(product);
-                _repo.Save();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
-            return View(product);
-        }
-
-        public IActionResult Delete(int id)
-        {
-            var product = _repo.GetById(id);
-            if (product == null) return NotFound();
-
-            return View(product);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeleteConfirmed(int id)
-        {
-            _repo.Delete(id);
-            _repo.Save();
-            return RedirectToAction("Index");
-        }
-
-        public IActionResult Details(int id)
-        {
-            var product = _repo.GetById(id);
-            if (product == null) return NotFound();
-            return View(product);
-        }
-    }
-
+		[HttpGet]
+		public IActionResult Index()
+		{
+			var products = _context.Products.ToList();
+			return View(products);
+		}
+	}
 }
---
+++ Web/ViewModels/Product/CreateProductViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.ViewModels.Product
+{
+	public class CreateProductViewModel
+	{
+		
+	}
+}
